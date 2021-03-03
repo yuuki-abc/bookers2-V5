4c55ca86ed812628e2c78fa50659d1aa6f00c41c
yuuki-abc <yuki.abc.300@gmail.com>
2021-03-03 18:13:11 +0000

    いいねの非同期通信化が完成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,6 +3,7 @@ class FavoritesController < ApplicationController
 
  def create
     @book = Book.find(params[:book_id])
+    @class = ".favorite-btn-#{@book.id}"
     favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
     # redirect_back(fallback_location: root_path)
@@ -10,9 +11,15 @@ class FavoritesController < ApplicationController
 
   def destroy
     @book = Book.find(params[:book_id])
+    @class = ".favorite-btn-#{@book.id}"
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
     # redirect_back(fallback_location: root_path)
   end
 
+  # def set_variables
+  #   @book = Book.find(params[:book_id])
+  #   @class = "favorite-btn-#{@book.id}"
+  #   @book_id = ".favorite-btn-#{@book.id}"
+  # end
 end
